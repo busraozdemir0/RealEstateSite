e8b57b1307b141f4d12b30a794abe6bb237f4e81
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-05-28 16:27:51 +0300

    Apinin consume edilmesi (ilanların ana sayfada listelenmesi)

--- RealEstate.UI/Program.cs
+++ RealEstate.UI/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpClient();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -22,6 +23,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Default}/{action=Index}/{id?}");
 
 app.Run();
--- RealEstate.UI/ViewComponents/HomePage/_DefaultHomePageProductList.cs
+++ RealEstate.UI/ViewComponents/HomePage/_DefaultHomePageProductList.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate.UI.DTOs.ProductDtos;
 
 namespace RealEstate.UI.ViewComponents.HomePage
 {
     public class _DefaultHomePageProductList:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultHomePageProductList(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient(); // istemci ornegi olusturuldu.
+            var responseMessage = await client.GetAsync("https://localhost:44314/api/Products/ProductListWithCategory");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); // Gelen icerigi string formatinda oku
+                var values = JsonConvert.DeserializeObject<List<ResultProductDtos>>(jsonData);   // DeserializeObject => json bir degeri okuyor ve bizim istedigimiz metin formatina donusturur
+                                                                                                 // SerializeObject => metin formatini json formatina donusturur.
+                return View(values);
+            }
             return View();
         }
     }
